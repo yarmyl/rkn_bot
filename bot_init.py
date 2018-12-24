60d325c5cdc47ecf81a13caca415ca39751db9ff
Yaroslav Mylov <leviafan.raven@mail.ru>
2018-12-24 17:27:59 +0300

    add show

--- bot_init.py
+++ bot_init.py
@@ -7,6 +7,15 @@ import configparser
 import os
 from netaddr import *
 from subprocess import Popen, PIPE
+import json
+
+
+def makeKeyboard(answers):
+    return json.dumps({
+        "resize_keyboard": True,
+        "one_time_keyboard": True,
+        "keyboard": [answers]
+    })
 
 
 def readToken(file_name):
@@ -16,12 +25,15 @@ def readToken(file_name):
         raise SystemExit("Fail to read token file")
     return file.read()
 
+
 def createParser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--conf', nargs='?')
     return parser
 
-def req(url, method, data=[], req_method=0):
+
+def req(url, method, data={}, req_method=0):
+    print(data)
     if not req_method:
         r = requests.get(url+method, data=data)
     elif req_method == 1:
@@ -33,6 +45,7 @@ def req(url, method, data=[], req_method=0):
     else:
         print("Wrong method!")
     return r.json()
+
 
 def getSettings(config):
     settings = dict()
@@ -49,13 +62,13 @@ class Bot:
     HELP = [
         '/start - Enjoy\n',
         '/help - This message\n',
-        '/show [cur|new] - Show statistics by new or current rules\n',
+        '/show [cur|new|tor|w] - Show statistics by new or current rules\n',
         '/check - Check Difference between current and new rules\n',
         '/apply - Apply new rules\n',
         '/update - Update new rules\n',
-        '/search [IP] [cur|new] - Search IP in current and old rules\n',
-        '/add [IP] - Proxy IP to tor\n',
-        '/del [IP] - Remove IP to tor\n'
+        '/search [IP] [cur|new|tor|w] - Search IP in current and old rules\n',
+        '/add [IP] [tor|w] - Add IP to list\n',
+        '/del [IP] [tor|w] - Remove IP from list\n'
     ]
 
     def __init__(self, conf):
@@ -63,6 +76,7 @@ class Bot:
         self.__update = conf['update']
         self.__ipset = conf['ipset']
         self.__url = "https://api.telegram.org/bot" + self.__token + '/'
+        self.rm_keyboard = json.dumps({"remove_keyboard": True})
         self.last_id = None
         self.updateRules()
         self.replyHi(self.getLastUpdate(self.getUpdates(None, 0)))
@@ -70,39 +84,63 @@ class Bot:
             raise SystemExit("Bad token")
 
     def checkToken(self):
-         return req(self.__url, "getMe")['ok']
+        return req(self.__url, "getMe")['ok']
 
     def replyHi(self, data):
         if data.get('message'):
-            self.sendMessage("Hi, sorry, I'm alive", \
-                data['message']['chat']['id'])
+            self.sendMessage(
+                "Hi, sorry, I'm alive",
+                data['message']['chat']['id']
+            )
 
     def updateRules(self):
         os.system('wget -O black_nets.list ' + self.__update)
-        
+
     def applyRules(self):
         os.system('cp black_nets.list cur.list')
         os.system('sudo ipset flush ' + self.__ipset)
-        os.system("cat cur.list | sed 's/^/add " + self.__ipset + " /g' > rules.list")
+        os.system(
+            "cat cur.list | sed 's/^/add " + self.__ipset +
+            " /g' > rules.list"
+        )
         os.system('sudo ipset restore < rules.list')
 
     def checkRules(self):
         self.updateRules()
-        out, err = Popen('diff cur.list black_nets.list', \
-            shell=True, stdout=PIPE).communicate()
-        return out
-        
+        out, err = Popen(
+            'diff cur.list black_nets.list',
+            shell=True,
+            stdout=PIPE
+        ).communicate()
+        net_add = 0
+        net_del = 0
+        for str in out.decode("utf-8"):
+            if str[0] == '>':
+                net_add += 1
+            elif str[0] == '<':
+                net_del += 1
+        return "Add {0[0]} nets and Del {0[1]} nets".format((net_add, net_del))
+
     def showRules(self, list):
-        file_name = 'cur.list' if list == 'cur' else 'black_net.list'
+        if list == 'cur':
+            file_name = 'cur.list'
+        elif list == 'w':
+            file_name = 'white.list'
+        elif list == 'tor':
+            file_name = 'tor.list'
+        else:
+            file_name = 'black_nets.list'
         try:
             file = open(file_name, 'r')
         except:
-            raise SystemExit("Fail to read token file")
-        net_list = []
+            raise SystemExit("Fail to read file")
+        count = 0
+        size = 0
         for net in file:
-            net_list.append(IPNetwork(net[:-1]))
-        cidr_merger(net_list)
-        print(net_list)
+            count += 1
+            size += IPNetwork(net[:-1]).size
+        return "{0:d} Networks and {2:d} IP-adressess in {1} list" \
+            .format(count, list, size)
 
     def getUpdates(self, offset=None, timeout=300):
         if not offset:
@@ -126,13 +164,29 @@ class Bot:
             last_update = {}
         return last_update
 
-    def sendMessage(self, text, chat_id):
+    def sendMessage(self, text, chat_id, board=None):
         return req(
             self.__url,
             "sendMessage",
-            {'chat_id': chat_id, 'text': text},
+            {'chat_id': chat_id, 'text': text, 'reply_markup': board},
             1
         )
+
+    def checkList(self, str, chat_id, buttons):
+        list = 0
+        if str not in buttons:
+            self.sendMessage(
+                "Please select list",
+                chat_id,
+                makeKeyboard(buttons)
+            )
+            for elem in self.getUpdates():
+                if elem.get('message').get('text') not in buttons:
+                    return 0
+                list = elem.get('message').get('text')
+        else:
+            list = str
+        return list
 
     def botBrain(self, text, chat_id):
         if text == "/help":
@@ -143,8 +197,15 @@ class Bot:
         elif text == "/start":
             self.sendMessage("Please, use /help", chat_id)
         elif text[:5] == "/show":
-            pass
-#            self.showRules()
+            list = self.checkList(text[6:], chat_id, ["cur", "new", "tor", "w"])
+            if list:
+                self.sendMessage(
+                    self.showRules(list),
+                    chat_id,
+                    self.rm_keyboard
+                )
+            else:
+                self.sendMessage("WTF?!", chat_id, self.rm_keyboard)
         elif text == "/check":
             self.sendMessage(self.checkRules(), chat_id)
         elif text == "/apply":
@@ -152,7 +213,7 @@ class Bot:
             self.sendMessage("It's Done!", chat_id)
         elif text == "/update":
             self.updateRules()
-            self.sendMessage("It's Done!", chat_id)
+            self.sendMessage("It's Done!", chat_id, )
         elif text[:7] == "/search":
             pass
         elif text[:4] == "/add":
