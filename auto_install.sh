d3eafb7868f0a12b79349ede3a11e6caecb024dd
Yaroslav Mylov <leviafan.raven@mail.ru>
2018-12-26 16:24:23 +0300

    fix autoinstall

--- auto_install.sh
+++ auto_install.sh
@@ -3,28 +3,28 @@
 cd "$(dirname "$0")"
 ### sudo sed -i 's/%sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL) NOPASSWD:ALL/g' /etc/sudoers
 touch cur.list tor.list white.list
-sudo apt update
+apt update
 
 # install python
 ### sudo apt install -y python3 python3-pip git
 ### pip3 install netaddr
 
 # install Docker
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
+apt install -y apt-transport-https ca-certificates curl software-properties-common ipset iptables
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
+apt-get update
+apt-get install -y docker-ce
 ### sudo usermod -aG docker $USERNAME
 
 # install tor
 git clone https://github.com/yarmyl/torproxy.git
 cd torproxy
-sudo docker build -t tor .
+docker build -t tor .
 cd ../
-sudo rm -r torproxy
-sudo docker run --name tor --net host -d --restart tor
-sudo ./iptables_rules.sh
+rm -r torproxy
+docker run --name tor --net host -d --restart tor
+./iptables_rules.sh
